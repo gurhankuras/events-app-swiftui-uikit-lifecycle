de57d4f994131255f9994d55ac8b1c218299c6d4
gurhankuras <gurhankuras@hotmail.com>
2022-03-23 19:12:11 +0300

    Initial Commit

---
+++ events-app-swiftui-uikit-lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  events-app-swiftui-uikit-lifecycle
+//
+//  Created by Gürhan Kuraş on 3/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
